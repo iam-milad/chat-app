0e21c4ab6cc43b151949f3b5a68a839f65f68447
iam-milad <miladnouri073@hotmail.com>
2025-08-12 22:47:10 +0100

    add backend folders and files

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,55 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    generateToken(newUser._id, res);
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.error("Signup error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const login = (req, res) => {
+  res.send("login route");
+};
+
+export const logout = (req, res) => {
+  res.send("logout route");
+};
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS - Cross-Site Scripting attacks (not accessible via JavaScript)
+    sameSite: "strict", // CSRF - Cross-Site Request Forgery Attacks
+    secure: process.env.NODE_ENV !== "development",
+  });
+
+  return token;
+};
